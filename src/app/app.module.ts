789ba50a3dd4e8eff07e37bd1c514db542df26e1
João Alves <joaoalves.contato@outlook.com.br>
2020-10-06 16:27:21 -0300

    Add Carousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BarComponent } from './bar/bar.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BarComponent
+    BarComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule
